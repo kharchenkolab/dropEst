0483f3883bf2a87ce1541f880341f89c728c1222
Viktor Petukhov <viktor.s.petuhov@ya.ru>
2016-09-17 14:43:52 +0000

    Improve broken merge

--- Estimation/Merge/BrokenRealBarcodesMergeStrategy.cpp
+++ Estimation/Merge/BrokenRealBarcodesMergeStrategy.cpp
@@ -20,7 +20,16 @@ namespace Merge
 		if (base_cell_ind == neighbour_cells[0])
 			return base_cell_ind;
 
-		return neighbour_cells[rand() % neighbour_cells.size()];
+		ul_list_t ids_distr;
+		for (auto id : neighbour_cells)
+		{
+			for (int i = 0; i < (size_t)std::sqrt(container.cell_genes(id).size()) + 1; ++i)
+			{
+				ids_distr.push_back(id);
+			}
+		}
+
+		return ids_distr[rand() % ids_distr.size()];
 	}
 
 	void BrokenRealBarcodesMergeStrategy::init(const Estimation::CellsDataContainer &container)
@@ -32,7 +41,7 @@ namespace Merge
 
 	long BrokenRealBarcodesMergeStrategy::get_max_merge_dist(long min_real_cb_dist) const
 	{
-		return min_real_cb_dist == 0 ? 0 : min_real_cb_dist + 15;
+		return min_real_cb_dist == 0 ? 0 : min_real_cb_dist + 10;
 	}
 }
 }
