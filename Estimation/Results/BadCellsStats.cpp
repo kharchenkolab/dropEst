e60b82efaa75874697fb956c356e4bf1b9939255
Viktor Petukhov <SuSsaHin@ya.ru>
2017-07-19 16:37:19 +0300

    Fixed reads_per_umig output

--- Estimation/Results/BadCellsStats.cpp
+++ Estimation/Results/BadCellsStats.cpp
@@ -16,25 +16,26 @@ namespace Estimation
 			Tools::trace_time("Writing R data to " + filename);
 			RInside *R = Tools::init_r();
 
-			ss_cnt_t genes_reads, genes_umis, reads_per_umig, reads_per_umi;
+			ss_cnt_t genes_reads, genes_umis;
+			std::unordered_map<std::string, ss_cnt_t> reads_per_umig;
 			for (auto cell_id : container.filtered_cells())
 			{
 				std::string cell_barcode = container.cell_barcode(cell_id);
 
 				auto &cell_umis = genes_umis[cell_barcode];
-				auto &cell_reads_p_umis = reads_per_umi[cell_barcode];
 				auto &cell_reads_p_umigs = reads_per_umig[cell_barcode];
 
 				for (auto const &gene_umis : container.cell_genes(cell_id))
 				{
 					cell_umis[gene_umis.first] = (unsigned)gene_umis.second.size();
 					auto &current_gene = genes_reads[cell_barcode][gene_umis.first];
+					auto &out_gene_umis = cell_reads_p_umigs[gene_umis.first];
+
 					for (auto const &umi_reads : gene_umis.second)
 					{
 						size_t read_count = umi_reads.second.read_count;
 						current_gene += read_count;
-						cell_reads_p_umis[umi_reads.first] += read_count;
-						cell_reads_p_umigs[umi_reads.first + gene_umis.first] = (unsigned) read_count;
+						out_gene_umis [umi_reads.first] = (unsigned) read_count;
 					}
 				}
 			}
@@ -65,8 +66,7 @@ namespace Estimation
 		            Named("merge_edit_distance") = wrap(container.stats().get_raw(Stats::MERGE_EDIT_DISTANCE_BY_CELL)),
 		            Named("merge_probs") = wrap(container.stats().get_raw(Stats::MERGE_PROB_BY_CELL)),
 		            Named("merge_targets") = merge_targets,
-		            Named("excluded_cells") = wrap(container.excluded_cells()),
-		            Named("umi_per_cell") = wrap(reads_per_umi) // TODO: Remove?
+		            Named("excluded_cells") = wrap(container.excluded_cells())
 			);
 
 			R->parseEvalQ("saveRDS(d, '" + filename + "')");
