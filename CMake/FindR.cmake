99d664b4413fe2c5efc32504eaf7bf97a80d5ee5
Viktor Petukhov <SuSsaHin@ya.ru>
2017-07-30 12:17:28 +0300

    Improved FindR.cmake

--- CMake/FindR.cmake
+++ CMake/FindR.cmake
@@ -1,7 +1,7 @@
 # FindR
 # --------
 #
-# Find R, RCpp and RInside
+# Find R, RCpp, RCpArmadillo and RInside
 #
 # You can set R_ROOT and R_PACKAGES to specify R paths
 #
@@ -10,38 +10,132 @@
 #
 # This module defines the following variables:
 #
-#   R_INCLUDE_DIRS - include directories for R
-#   R_LIBRARIES - libraries to link against R
-#   R_FOUND - true if R has been found and can be used
+#   R_INCLUDE_DIRS: include directories for R
+#   R_LIBRARIES: libraries to link against R
+#   R_PACKAGES_DIRS: directories with installed R packages
+#   R_FOUND: true if R has been found and can be used
 
-find_path(R_INCLUDE_DIR R.h PATHS ${R_ROOT}/include /usr/lib/R/include/ /usr/share/R/include)
-find_library(R_LIBRARY NAME R PATHS ${R_ROOT}/lib PATH_SUFFIXES R/lib lib)
+# detection for OSX
+if(APPLE)
+    find_library(R_LIBRARIES R)
+    if(R_LIBRARIES)
+        set(R_ROOT "${R_LIBRARIES}/Resources" CACHE PATH "R root directory")
+        set(R_INCLUDE_DIRS "${R_ROOT}/include" CACHE PATH "R include directory")
+        set(R_EXECUTABLE "${R_ROOT}/R" CACHE PATH "R executable")
+    endif()
+# Find R executable and paths (UNIX)
+else()
+    # find executable
+    if(NOT R_ROOT)
+        find_program(R_EXECUTABLE R)
+    else()
+        find_program(R_EXECUTABLE R
+                HINTS ${LIBRARY_ARCH_HINT_PATH} ${R_ROOT}/bin)
+    endif()
 
-get_filename_component(R_LIBRARY ${R_LIBRARY} REALPATH)
-get_filename_component(R_LIB_DIR ${R_LIBRARY} PATH)
-set(R_LIBS_PATHS ${R_ROOT} ${R_ROOT}/library ${R_LOCAL_LIBS} ${R_ROOT}/site-library ${R_PACKAGES} ${R_LIB_DIR}/../ ${R_LIB_DIR}/../../ /usr/local/lib /usr/lib)
+    if(R_EXECUTABLE-NOTFOUND)
+        message(STATUS "Unable to locate R executable")
+    endif()
 
+    # ask R for the home path
+    if(NOT R_ROOT)
+        execute_process(
+                COMMAND ${R_EXECUTABLE} "--slave" "--vanilla" "-e" "cat(R.home())"
+                OUTPUT_VARIABLE R_ROOT
+        )
+        if(R_ROOT)
+            set(R_ROOT ${R_ROOT} CACHE PATH "R root directory")
+        endif()
+    endif()
+
+    # ask R for the include dir
+    if(NOT R_INCLUDE_DIRS)
+        execute_process(
+                COMMAND ${R_EXECUTABLE} "--slave" "--no-save" "-e" "cat(R.home('include'))"
+                OUTPUT_VARIABLE R_INCLUDE_DIRS
+        )
+        if(R_INCLUDE_DIRS)
+            set(R_INCLUDE_DIRS ${R_INCLUDE_DIRS} CACHE PATH "R include directory")
+        endif()
+    endif()
+
+    # ask R for the lib dir
+    if(NOT R_LIB_DIR)
+        execute_process(
+                COMMAND ${R_EXECUTABLE} "--slave" "--no-save" "-e" "cat(R.home('lib'))"
+                OUTPUT_VARIABLE R_LIB_DIR
+        )
+    endif()
+
+    # look for the core R library
+    find_library(R_CORE_LIBRARY NAMES R
+            HINTS ${R_LIB_DIR} ${R_ROOT}/bin)
+    if(R_CORE_LIBRARY)
+        set(R_LIBRARIES ${R_CORE_LIBRARY})
+    else()
+        message(STATUS "Could not find libR shared library.")
+    endif()
+
+    # cache R_LIBRARIES
+    if(R_LIBRARIES)
+        set(R_LIBRARIES ${R_LIBRARIES} CACHE PATH "R runtime libraries")
+    endif()
+endif()
+
+# look for necessary R packages
+if (NOT R_PACKAGES_DIRS)
+    execute_process(
+            COMMAND ${R_EXECUTABLE} "--slave" "--no-save" "-e" "cat(.libPaths(), sep=';')"
+            OUTPUT_VARIABLE R_PACKAGES_DIRS
+    )
+endif()
 
 find_path(RCPP_ARM_INCLUDE_DIR RcppArmadillo.h
-        PATHS ${R_LIBS_PATHS}
-        PATH_SUFFIXES R/library/RcppArmadillo/include/ R/site-library/RcppArmadillo/include/ RcppArmadillo/include/)
+        HINTS ${R_PACKAGES_DIRS}
+        PATH_SUFFIXES /RcppArmadillo/include/)
 find_path(RCPP_INCLUDE_DIR Rcpp.h
-        PATHS ${R_LIBS_PATHS}
-        PATH_SUFFIXES R/library/Rcpp/include/ R/site-library/Rcpp/include/ Rcpp/include/)
+        HINTS ${R_PACKAGES_DIRS}
+        PATH_SUFFIXES /Rcpp/include)
 find_path(RINSIDE_INCLUDE_DIR RInside.h
-        PATHS ${R_LIBS_PATHS}
-        PATH_SUFFIXES R/library/RInside/include R/site-library/RInside/include RInside/include)
+        HINTS ${R_PACKAGES_DIRS}
+        PATH_SUFFIXES /RInside/include)
 
 find_library(RINSIDE_LIBRARY RInside PATH ${RINSIDE_INCLUDE_DIR}/../lib)
 
-include(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(R
-        REQUIRED_VARS R_LIBRARY R_INCLUDE_DIR RCPP_INCLUDE_DIR RCPP_ARM_INCLUDE_DIR RINSIDE_INCLUDE_DIR RINSIDE_LIBRARY)
+# Additional packages
+execute_process(
+        COMMAND ${R_EXECUTABLE} "--slave" "--no-save" "-e" "cat('Matrix' %in% rownames(installed.packages()))"
+        OUTPUT_VARIABLE R_MATRIX_PKG
+)
 
-if (R_FOUND)
-    set(R_INCLUDE_DIRS ${R_INCLUDE_DIR} ${RCPP_INCLUDE_DIR} ${RCPP_ARM_INCLUDE_DIR} ${RINSIDE_INCLUDE_DIR})
-    set(R_LIBRARIES ${R_LIBRARY} ${RINSIDE_LIBRARY})
+if(${R_MATRIX_PKG} STREQUAL FALSE)
+    message(STATUS "Unable to locate R package 'Matrix'")
+    set(R_MATRIX_PKG )
 endif()
 
+# define find requirements
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(R DEFAULT_MSG
+        R_LIBRARIES
+        R_INCLUDE_DIRS
+        RCPP_INCLUDE_DIR
+        RCPP_ARM_INCLUDE_DIR
+        RINSIDE_INCLUDE_DIR
+        RINSIDE_LIBRARY
+        R_MATRIX_PKG
+)
 
-mark_as_advanced(R_INCLUDE_DIR R_LIBRARY RCPP_INCLUDE_DIR RINSIDE_INCLUDE_DIR RINSIDE_LIBRARY)
+if (R_FOUND)
+    set(R_INCLUDE_DIRS ${R_INCLUDE_DIRS} ${RCPP_INCLUDE_DIR} ${RCPP_ARM_INCLUDE_DIR} ${RINSIDE_INCLUDE_DIR})
+    set(R_LIBRARIES ${R_LIBRARIES} ${RINSIDE_LIBRARY})
+    message(STATUS "Found R: ${R_ROOT}")
+endif()
+
+# mark low-level variables from FIND_* calls as advanced
+mark_as_advanced(
+        R_CORE_LIBRARY
+        RCPP_ARM_INCLUDE_DIR
+        RCPP_INCLUDE_DIR
+        RINSIDE_INCLUDE_DIR
+        R_MATRIX_PKG
+)
