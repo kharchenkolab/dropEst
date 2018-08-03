30c945749c1ad5e406ea3eebda211bfacbbc7d45
Ben Evans <benjamin.robert.evans@gmail.com>
2018-08-03 11:51:01 -0400

    fixed boost test include for newer versions of boost. see https://www.boost.org/doc/libs/1_67_0/libs/test/doc/html/boost_test/tests_organization/test_cases/test_organization_nullary.html for more details.

--- Tests/TestEstimation.cpp
+++ Tests/TestEstimation.cpp
@@ -1,7 +1,7 @@
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE tests
 
-#include <boost/test/unit_test.hpp>
+#include <boost/test/included/unit_test.hpp>
 
 #include "Estimation/CellsDataContainer.h"
 
